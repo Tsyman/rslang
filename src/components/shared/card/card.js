6ab81af3c8cd42bb50903738f9a6a65c758be0bb
Sergey0s <mediaworld@mail.ru>
2020-06-26 01:34:51 +0300

    feat: complete word card

--- src/components/shared/card/card.js
+++ src/components/shared/card/card.js
@@ -1,7 +1,5 @@
 import './card.scss';
 
-const content = document.querySelector('.content');
-
 class Card {
   constructor(startText, endText, word, textTranslate, wordType, wordTranslate) {
     this.startText = startText;
@@ -10,18 +8,16 @@ class Card {
     this.textTranslate = textTranslate;
     this.wordType = wordType;
     this.wordTranslate = wordTranslate;
-    // eslint-disable-next-line no-nested-ternary
-    this.textClass = (startText + endText + word).length > 84 ? 'nanoSize' : (startText + endText + word).length > 60 ? 'microSize'
-      : (startText + endText + word).length > 35 && (startText + endText + word).length <= 50 ? 'smallSize' : '';
+    this.textClass = '';
+    if ((startText + endText + word).length > 84) { this.textClass = 'nanoSize'; } else if ((startText + endText + word).length > 60) { this.textClass = 'microSize'; } else if ((startText + endText + word).length > 35 && (startText + endText + word).length <= 60) { this.textClass = 'smallSize'; }
   }
 
   createCard = () => {
-    console.log(this.textClass);
-
     const wordCard = document.createElement('div');
     wordCard.innerHTML = `
     <div class="card__wrapper">
              <div class="card__container">
+             <img class="card__img" src="../../../assets/images/image-icon.png" alt="image">
              <div class="card__containerWrapper">
                   <div class="card__text ${this.textClass}">
                         <div class="card__startText">${this.startText}</div>
@@ -37,16 +33,28 @@ class Card {
                    </div>
                 </div>
      </div>
+     <div class="card__bottomContent">
         <div class="card__wordTranslate">${this.wordTranslate}</div>
+            <div class="card__bottomContentBtn">
+            <div class="card__btnAgain">Снова</div>
+            <div class="card__btnHard">Трудно</div>
+            <div class="card__btnGood">Хорошо</div>
+            <div class="card__btnEasy">Легко</div>
+</div>
+        </div>
     </div>`;
     return wordCard;
   }
 }
 
-const newCard = new Card('I would like a room with a sea', '.', 'view', 'Я бы хотел комнату с видом на море', 'сущ.ед.ч.', 'вид, пейзаж');
-const newCard2 = new Card('I ', 'would like a room with a sea view.', 'would', 'Я бы хотел комнату с видом на море', 'сущ.ед.ч.', 'бы');
-const newCard3 = new Card('I ', 'would liwwwww ule wouldlike wou like would like w a room with a sea view.', 'would', 'Я бы хотел комнату с видом на море', 'сущ.ед.ч.', 'бы');
+export default Card;
 
-content.append(newCard.createCard());
-// content.append(newCard2.createCard())
-// content.append(newCard3.createCard())
+/*
+Тестовые карточки разной длины;
+const newCard = new Card('I would like a room with a sea',
+'.', 'view', 'Я бы хотел комнату с видом на море', 'сущ.ед.ч.', 'вид, пейзаж');
+const newCard2 = new Card('I ', 'would like a room with a sea view.',
+ 'would', 'Я бы хотел комнату с видом на море', 'сущ.ед.ч.', 'бы');
+const newCard3 = new Card('I ', 'would liwwwww ule wouldlike wou like would
+ like w a room with a sea view.', 'would', 'Я бы хотел комнату с видом на море', 'сущ.ед.ч.', 'бы');
+*/
