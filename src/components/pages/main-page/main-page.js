313fbaf20824cdebff34936e45f919e818e96e79
AnzhelikaKurnikova <anzhelika.kurnikova@vicuesoft.com>
2020-06-26 16:14:27 +0300

    feat: add layout for main page

--- src/components/pages/main-page/main-page.js
+++ src/components/pages/main-page/main-page.js
@@ -17,8 +17,39 @@ class MainPage {
   }
 
   view = `
-    <section class="section">
+    <section class="section-main-page">
+      <div class="section-main-page__banner">
+        Hello, <span id="section-main-page--name"> Someone</span>! Давай заниматься.
+        <div class="section-main-page__banner--icon">
+        </div>
+      </div>
+      <div class="section-main-page__menu">
+        <div class="section-main-page__menu--start-studying">
+          <div class="section-main-page__menu--start-studying-text">
+            Good evening! - Добрый вечер!
+          </div>
+          <div class="section-main-page__menu--start-studying-button">
+            Выучить пару английских слов
+          </div>
+        </div>
+        <div class="section-main-page__menu--statistics">
+          <div class="section-main-page__menu--statistics-heading">
+            Статистика
+          </div>
+          <div class="section-main-page__menu--statistics-text">
+            Похоже, что вы только что зарегистрировались или очень давно не занимались
+          </div>
+          <div class="section-main-page__menu--statistics-button">
+            Го играть!
+          </div>
+          <div class="section-main-page__menu--statistics-icon">
+          </div>
+        </div>
+      </div>
     </section>
+
+
+
     <div class="popup" id="popup">
       <div class="popup-content" id="popup-content">
         <div class="popup-content__control-section">
