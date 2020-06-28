eeadf7713412f70a30f34a8842347acdc33e3c69
Max Tsyman <xxxmaximxxx777@gmail.com>
2020-06-28 13:19:53 +0300

    refactor: delete one module

--- src/components/pages/authorization-page/tokenHandling/tokenLifetime.js
+++
@@ -1,8 +0,0 @@
-function tokenLifetime() {
-  const lifetime = 4;
-  const currentTIme = new Date();
-  const tokenEndTime = currentTIme.setHours(currentTIme.getHours() + lifetime);
-  return new Date(tokenEndTime).toLocaleTimeString();
-}
-
-export default tokenLifetime;
