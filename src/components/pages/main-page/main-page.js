f2691b7410f0fd59405ef526daab95d75074b7a6
AnzhelikaKurnikova <anzhelika.kurnikova@vicuesoft.com>
2020-07-02 11:01:32 +0300

    fix: delete conflict code

--- src/components/pages/main-page/main-page.js
+++ src/components/pages/main-page/main-page.js
@@ -1,19 +1,6 @@
 import './main-page.scss';
 
 class MainPage {
-  popupOpenButton = null;
-
-  popup = null;
-
-  popupContent = null;
-
-  popupCloseButton = null;
-
-  constructor() {
-    this.openPopupFunction = this.openPopupFunction.bind(this);
-    this.closePopupFunction = this.closePopupFunction.bind(this);
-  }
-
   view = `
           <header class="header-main">
             <div class="container">
@@ -50,30 +37,6 @@ class MainPage {
 
   async render() {
     return this.view;
-  }
-
-  async afterRender() {
-    this.popupOpenButton = document.querySelector('#open-popup');
-    this.popup = document.querySelector('#popup');
-    this.popupContent = document.querySelector('#popup-content');
-    this.popupCloseButton = document.querySelector('#popup-close-button');
-
-    this.popupOpenButton.addEventListener('click', this.openPopupFunction);
-    this.popupCloseButton.addEventListener('click', this.closePopupFunction);
-  }
-
-  openPopupFunction() {
-    this.popup.style.opacity = '1';
-    this.popup.style.visibility = 'visible';
-    this.popupContent.style.opacity = '1';
-    this.popupContent.style.transform = 'translate(-50%, -50%) scale(1)';
-  }
-
-  closePopupFunction() {
-    this.popup.style.opacity = '0';
-    this.popup.style.visibility = 'hidden';
-    this.popupContent.style.opacity = '0';
-    this.popupContent.style.transform = 'translate(-50%, -50%) scale(.25)';
   }
 }
 
