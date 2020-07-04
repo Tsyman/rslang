2a91de7f17d643a30471a142e2529efbc129c528
MariaSyrokvash <mariya.syrokvash@gmail.com>
2020-07-04 14:39:21 +0300

    feat: add audio-call-game start page

--- src/components/minigames/audiocall/audiocall.js
+++ src/components/minigames/audiocall/audiocall.js
@@ -1,12 +1,41 @@
 import './audiocall.scss';
 
 class Audiocall {
+  audioCallContainer = null;
+
+  startGameButton = null;
+
+  mainGamePage = `
+    <div>Game</div>
+  `
+
+  constructor() {
+    this.goToMainGamePage = this.goToMainGamePage.bind(this);
+  }
+
   view = `
-        Hello World!
+        <section class="audiocall-start" id="audio-call-container">
+          <div class="audiocall-start__container">
+            <div class="audiocall-start__wrapper">
+              <p class="audiocall-start__title">Аудиовызов</p>
+              <div class="audiocall-start__button" id="start-game-button">Начать игру</div>
+            </div>
+          </div>
+        </section>
         `;
 
   async render() {
     return this.view;
+  }
+
+  async afterRender() {
+    this.audioCallContainer = document.getElementById('audio-call-container');
+    this.startGameButton = document.getElementById('start-game-button');
+    this.startGameButton.addEventListener('click', this.goToMainGamePage);
+  }
+
+  goToMainGamePage() {
+    this.audioCallContainer.innerHTML = this.mainGamePage;
   }
 }
 
