4fb65e01f8c4ecd4e138d4ed3e5bb356b9c7a309
MariaSyrokvash <mariya.syrokvash@gmail.com>
2020-07-04 21:47:35 +0300

    feat: add page with game

--- src/components/minigames/audiocall/audiocall.js
+++ src/components/minigames/audiocall/audiocall.js
@@ -6,7 +6,40 @@ class Audiocall {
   startGameButton = null;
 
   mainGamePage = `
-    <div>Game</div>
+    <div class="audiocall-start__container">
+      <div class="audiocall-game__wrapper">
+        <div class="audiocall-game__inner">
+          <div class="audiocall-game__sound audiocall-game__sound-bg">
+            <img class="audiocall-game__img" src="../../../assets/images/audio-call-game-icon.svg">
+          </div>
+          <div class="audiocall-game__english-word visually-hidden">lorem ipsum</div>
+        </div>
+        <ul class="audiocall-game__list">
+          <li class="audiocall-game__item">
+            <p class="audiocall-game__number">1</p>
+            <p class="audiocall-game__word">Lorem</p>
+          </li>
+          <li class="audiocall-game__item audiocall-game__item-incorrect">
+            <p class="audiocall-game__number">2</p>
+            <p class="audiocall-game__word">Lorem</p>
+          </li>
+          <li class="audiocall-game__item">
+            <p class="audiocall-game__number">3</p>
+            <p class="audiocall-game__word">Lorem</p>
+          </li>
+          <li class="audiocall-game__item">
+            <p class="audiocall-game__number">4</p>
+            <p class="audiocall-game__word">Lorem</p>
+          </li>
+          <li class="audiocall-game__item">
+            <p class="audiocall-game__number">5</p>
+            <p class="audiocall-game__word">Lorem</p>
+          </li>
+        </ul>
+        <button class="audiocall-game__btn audiocall-game__btn-next">Не знаю</button>
+      </div>
+      </div>
+    </div>
   `
 
   constructor() {
