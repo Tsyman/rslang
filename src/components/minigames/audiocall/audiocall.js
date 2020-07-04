27ae094bc2bb15b9918856b9bbdbb0a4b9097b58
AnzhelikaKurnikova <anzhelika.kurnikova@vicuesoft.com>
2020-07-04 12:40:23 +0300

    feat: add code for changing from start page to main page

--- src/components/minigames/audiocall/audiocall.js
+++ src/components/minigames/audiocall/audiocall.js
@@ -1,12 +1,34 @@
 import './audiocall.scss';
 
 class Audiocall {
+  audioCallContainer = null;
+
+  startGameButton = null;
+
+  mainGamePage = '<div>Game</div>';
+
+  constructor() {
+    this.goToMainGamePage = this.goToMainGamePage.bind(this);
+  }
+
   view = `
-        Hello World!
+          <section id="audio-call-container">
+            <div id="start-game-button">Start</div>
+          </section>
         `;
 
   async render() {
     return this.view;
+  }
+
+  async afterRender() {
+    this.audioCallContainer = document.getElementById('audio-call-container');
+    this.startGameButton = document.getElementById('start-game-button');
+    this.startGameButton.addEventListener('click', this.goToMainGamePage);
+  }
+
+  goToMainGamePage() {
+    this.audioCallContainer.innerHTML = this.mainGamePage;
   }
 }
 
