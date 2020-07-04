989822c442b9ee1a5e140e6580ae562452a887d1
AnzhelikaKurnikova <anzhelika.kurnikova@vicuesoft.com>
2020-07-04 13:21:28 +0300

    feat: implement logic of confirm exit popup

--- src/components/minigames/audiocall/audiocall.js
+++ src/components/minigames/audiocall/audiocall.js
@@ -7,13 +7,43 @@ class Audiocall {
 
   mainGamePage = '<div>Game</div>';
 
+  closeGameButton = null;
+
+  closeConfirmExitPopupButton = null;
+
+  confirmExitFromGameButton = null;
+
+  confirmExitPopup = null;
+
+  confirmExitPopupContent = null;
+
+  urlForHomePage = '/';
+
   constructor() {
     this.goToMainGamePage = this.goToMainGamePage.bind(this);
+    this.openConfirmExitPopup = this.openConfirmExitPopup.bind(this);
+    this.closeConfirmExitPopup = this.closeConfirmExitPopup.bind(this);
+    this.goToMainWebsitePage = this.goToMainWebsitePage.bind(this);
   }
 
   view = `
-          <section id="audio-call-container">
+          <section id="audio-call-container" class="audiocall__container">
             <div id="start-game-button">Start</div>
+            <div class="audiocall__close-game-button" id="audiocall__close-game-button">
+              &times;
+            </div>
+            <div class="audiocall__confirm-exit-popup" id="audiocall__confirm-exit-popup">
+              <div class="audiocall__confirm-exit-popup-content" id="audiocall__confirm-exit-popup-content">
+                <div class="audiocall__confirm-exit-popup-content__heading">
+                  Игра не закончена!
+                </div>
+                <div class="audiocall__confirm-exit-popup-content__text">
+                  Если вы выйдете из игры, прогресс будет потерян.
+                </div>
+                <a class="btn btn--animated btn--green" id="audiocall__confirm-exit-popup-content__close-game">Закрыть</a>
+                <a class="btn btn--animated btn--green" id="audiocall__confirm-exit-popup-content__close-popup">Отмена</a>
+              </div>
+            </div>
           </section>
         `;
 
@@ -24,11 +54,37 @@ class Audiocall {
   async afterRender() {
     this.audioCallContainer = document.getElementById('audio-call-container');
     this.startGameButton = document.getElementById('start-game-button');
+    this.closeGameButton = document.getElementById('audiocall__close-game-button');
+    this.confirmExitPopup = document.getElementById('audiocall__confirm-exit-popup');
+    this.confirmExitPopupContent = document.getElementById('audiocall__confirm-exit-popup-content');
+    this.closeConfirmExitPopupButton = document.getElementById('audiocall__confirm-exit-popup-content__close-popup');
+    this.confirmExitFromGameButton = document.getElementById('audiocall__confirm-exit-popup-content__close-game');
     this.startGameButton.addEventListener('click', this.goToMainGamePage);
+    this.closeGameButton.addEventListener('click', this.openConfirmExitPopup);
+    this.closeConfirmExitPopupButton.addEventListener('click', this.closeConfirmExitPopup);
+    this.confirmExitFromGameButton.addEventListener('click', this.goToMainWebsitePage);
   }
 
   goToMainGamePage() {
     this.audioCallContainer.innerHTML = this.mainGamePage;
+  }
+
+  openConfirmExitPopup() {
+    this.confirmExitPopup.style.opacity = '1';
+    this.confirmExitPopup.style.visibility = 'visible';
+    this.confirmExitPopupContent.style.opacity = '1';
+    this.confirmExitPopupContent.style.transform = 'translate(-50%, -50%) scale(1)';
+  }
+
+  closeConfirmExitPopup() {
+    this.confirmExitPopup.style.opacity = '0';
+    this.confirmExitPopup.style.visibility = 'hidden';
+    this.confirmExitPopupContent.style.opacity = '0';
+    this.confirmExitPopupContent.style.transform = 'translate(-50%, -50%) scale(.25)';
+  }
+
+  goToMainWebsitePage() {
+    window.location.href = this.urlForHomePage;
   }
 }
 
