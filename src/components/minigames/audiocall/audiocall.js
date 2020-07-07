efd0d8b26e28c9e4994988e25aeb74a0c6526dd3
AnzhelikaKurnikova <anzhelika.kurnikova@vicuesoft.com>
2020-07-07 19:36:23 +0300

    feat: open results popup on statistics button

--- src/components/minigames/audiocall/audiocall.js
+++ src/components/minigames/audiocall/audiocall.js
@@ -36,6 +36,8 @@ class Audiocall {
 
   mySwiper = null;
 
+  resultsSwiper = null;
+
   countSlides = null;
 
   constructor() {
@@ -43,10 +45,9 @@ class Audiocall {
     this.openConfirmExitPopup = this.openConfirmExitPopup.bind(this);
     this.closeConfirmExitPopup = this.closeConfirmExitPopup.bind(this);
     this.goToMainWebsitePage = this.goToMainWebsitePage.bind(this);
-    this.openStatisticsFirstPage = this.openStatisticsFirstPage.bind(this);
     this.goToStartGamePage = this.goToStartGamePage.bind(this);
-    this.openStatisticsSecondPage = this.openStatisticsSecondPage.bind(this);
     this.renderGameSlides = this.renderGameSlides.bind(this);
+    this.openResultsPopup = this.openResultsPopup.bind(this);
   }
 
   popup = `
@@ -66,37 +67,17 @@ class Audiocall {
               </div>
             </div>`;
 
-  mainGamePage = `
-      <div class="audiocall-start__container" id="audiocall__swiper-container">
-        <div class="swiper-container">
+  resultsContainer = `
+    <section class="audiocall-statistics" id="results-popup">
+      <div class="audiocall-statistics__popup">
+        <div class="audiocall-statistics__container">
+          <p class="audiocall-statistics__title">Неплохо, но есть над чем поработать</p>
+          <div class="swiper-container s2">
             <div class="swiper-wrapper">
-            </div>
-            <div class="swiper-pagination"></div>
-        </div>
-        <div class="audiocall-game__btn-inner">
-          <button class="audiocall-game__btn">Не знаю</button>
-          <button class="audiocall-game__btn-next" id="button-next">Дальше</button>
-        </div>
-
-
-        <button id="result-1">Статистика1</button>
-        <button id="result-2">Статистика2</button>
-        <section class="audiocall-statistics" id="statistics-popup-1">
-          <div class="audiocall-statistics__popup">
-            <div class="audiocall-statistics__container">
-              <p class="audiocall-statistics__title">Неплохо, но есть над чем поработать</p>
-              <div class="audiocall-statistics__img"></div>
-              <div class="audiocall-statistics__btn-inner">
-                <a class="audiocall-statistics__play-btn audiocall-statistics__btn" id="audiocall-statistics__play-btn">Сыграем еще?</a>
-                <a class="audiocall-statistics__exit-btn audiocall-statistics__btn" id="audiocall-statistics__exit-btn">Выход из игры</a>
+              <div class="swiper-slide">
+                <div class="audiocall-statistics__img"></div>
               </div>
-            </div>
-          </div>
-        </section>
-        <section class="audiocall-statistics" id="statistics-popup-2">
-          <div class="audiocall-statistics__popup">
-            <div class="audiocall-statistics__container">
-                <p class="audiocall-statistics__title">Неплохо, но есть над чем поработать</p>
+              <div class="swiper-slide">
                 <div class="audiocall-statistics__content">
                   <div class="audiocall-statistics__content-mistake">
                     <div class="audiocall-statistics__content-mistake-inner">
@@ -129,14 +110,33 @@ class Audiocall {
                     </div>
                   </div>
                 </div>
-                <div class="audiocall-statistics__btn-inner">
-                  <a class="audiocall-statistics__play-btn audiocall-statistics__btn" id="audiocall-statistics__play-btn">Сыграем еще?</a>
-                  <a class="audiocall-statistics__exit-btn audiocall-statistics__btn" id="audiocall-statistics__exit-btn">Выход из игры</a>
-                </div>
+              </div>
             </div>
+            <div class="swiper-pagination"></div>
           </div>
-        </section>
+          <div class="audiocall-statistics__btn-inner">
+            <a class="audiocall-statistics__play-btn audiocall-statistics__btn" id="audiocall-statistics__play-btn">Сыграем еще?</a>
+            <a class="audiocall-statistics__exit-btn audiocall-statistics__btn" id="audiocall-statistics__exit-btn">Выход из игры</a>
+          </div>
+        </div>
       </div>
+    </section>
+  `;
+
+  mainGamePage = `
+      <div class="audiocall-start__container" id="audiocall__swiper-container">
+        <div class="swiper-container s1">
+            <div class="swiper-wrapper">
+            </div>
+            <div class="swiper-pagination"></div>
+        </div>
+        <div class="audiocall-game__btn-inner">
+          <button class="audiocall-game__btn">Не знаю</button>
+          <button class="audiocall-game__btn-next" id="button-next">Дальше</button>
+        </div>
+
+
+        <button id="result-1">Статистика1</button>
   `;
 
   view = `
@@ -162,18 +162,17 @@ class Audiocall {
 
   goToMainGamePage() {
     this.audioCallContainer.innerHTML = this.mainGamePage;
-    this.satisticsFirstButton = document.getElementById('result-1');
-    this.satisticsSecondButton = document.getElementById('result-2');
-    this.statisticsPopup = document.getElementById('statistics-popup-1');
-    this.statisticPlayButton = document.getElementById('audiocall-statistics__play-btn');
-    this.statisticExitButton = document.getElementById('audiocall-statistics__exit-btn');
-    this.statisticPlayButton.addEventListener('click', this.goToStartGamePage);
-    this.statisticExitButton.addEventListener('click', this.goToMainWebsitePage);
-    this.satisticsFirstButton.addEventListener('click', this.openStatisticsFirstPage);
-    this.statisticsPopupOpositePage = document.getElementById('statistics-popup-2');
-    this.satisticsSecondButton.addEventListener('click', this.openStatisticsSecondPage);
+    /*
+      this.statisticPlayButton = document.getElementById('audiocall-statistics__play-btn');
+      this.statisticExitButton = document.getElementById('audiocall-statistics__exit-btn');
+      this.statisticPlayButton.addEventListener('click', this.goToStartGamePage);
+      this.statisticExitButton.addEventListener('click', this.goToMainWebsitePage);
+      this.statisticsPopupOpositePage = document.getElementById('statistics-popup-2
+    */
     const previousHTML = this.audioCallContainer.innerHTML;
     this.audioCallContainer.innerHTML = (previousHTML + this.popup);
+    const previousHTML2 = this.audioCallContainer.innerHTML;
+    this.audioCallContainer.innerHTML = (previousHTML2 + this.resultsContainer);
     this.closeGameButton = document.getElementById('audiocall__close-game-button');
     this.confirmExitPopup = document.getElementById('audiocall__confirm-exit-popup');
     this.confirmExitPopupContent = document.getElementById('audiocall__confirm-exit-popup-content');
@@ -183,7 +182,7 @@ class Audiocall {
     this.closeGameButton.addEventListener('click', this.openConfirmExitPopup);
     this.closeConfirmExitPopupButton.addEventListener('click', this.closeConfirmExitPopup);
     this.confirmExitFromGameButton.addEventListener('click', this.goToMainWebsitePage);
-    this.mySwiper = new Swiper('.swiper-container', {
+    this.mySwiper = new Swiper('.s1', {
       direction: 'horizontal',
       loop: false,
       slidesPerView: 1,
@@ -201,6 +200,9 @@ class Audiocall {
     this.goToNextSlideButton.addEventListener('click', () => {
       this.goToNextSlide(this.mySwiper);
     });
+    this.satisticsFirstButton = document.getElementById('result-1');
+    this.satisticsFirstButton.addEventListener('click', this.openResultsPopup);
+    this.statisticsPopup = document.getElementById('results-popup');
   }
 
   openConfirmExitPopup() {
@@ -225,12 +227,22 @@ class Audiocall {
     window.location.href = this.urlForHomePageGame;
   }
 
-  openStatisticsFirstPage() {
+  openResultsPopup() {
+    this.resultsSwiper = new Swiper('.s2', {
+      direction: 'horizontal',
+      loop: false,
+      slidesPerView: 1,
+      spaceBetween: 20,
+      updateOnWindowResize: true,
+      grabCurcor: false,
+      simulateTouch: false,
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+    });
     this.statisticsPopup.style.display = 'block';
-  }
-
-  openStatisticsSecondPage() {
-    this.statisticsPopupOpositePage.style.display = 'block';
+    console.log('Click');
   }
 
   renderGameSlides(whereToAppend) {
