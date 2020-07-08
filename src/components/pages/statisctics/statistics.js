7b98ef81ebb00d50092b1cec15f9bdb361231087
valeriia_iavorskaia <valeriia_iavorskaia@epam.com>
2020-07-08 14:50:28 +0300

    changed structure

--- src/components/pages/statisctics/statistics.js
+++ src/components/pages/statisctics/statistics.js
@@ -10,35 +10,49 @@ class Statistics {
     this.longSeriesCorrectAnswers = longSeriesCorrectAnswers;
   }
 
-createStatistic = () => {
-  const statistisLayout = document.createElement('section');
+  view = '<section class="statistics"></section>';
 
-  statistisLayout.innerHTML = `
-  <section class="statistics">
-<div class="wrapper__statistics">
-    <div class="statistic">
-        <div class="statistic__icon"><img src="../../../assets/images/statistics/gym__statistics.svg" alt="Gym image" class="statistic__image"></div>
-        <h3 class="statistic__subtitle">Тренировка сегодня!</h3>
-        <div class="statistic__container">
-            <span class="statistic__details">Карточек завершено:</span><h5 class="statististic__details--align_left">${this.cardCompleted}</h5><br>
-            <span class="statistic__details">Правильные ответы:</span><h5 class="statististic__details--align_left">${this.rightAnswers}</h5><br>
-            <span class="statistic__details">Новые слова: </span><h5 class="statististic__details--align_left">${this.newWords}</h5><br>
-            <span class="statistic__details">Самая длинная серия правильных ответов: </span><h5 class="statististic__details--align_left">${this.longSeriesCorrectAnswers}</h5>
-        </div>
-    </div>
-    <div class="graph" id="graph__id">123123</div>
-</div>
-</section>
-  `;
-  return statistisLayout;
+  createStatistic = () => {
+    const statistisLayout = document.querySelector('.statistics');
+
+    if (statistisLayout) {
+      statistisLayout.innerHTML = `
+      <div class="wrapper__statistics">
+          <div class="statistic">
+              <div class="statistic__icon"><img src="../../../assets/images/statistics/gym__statistics.svg" alt="Gym image" class="statistic__image"></div>
+              <h3 class="statistic__subtitle">Тренировка сегодня!</h3>
+              <div class="statistic__container">
+                  <span class="statistic__details">Карточек завершено:</span><h5 class="statististic__details--align_left">${this.cardCompleted}</h5><br>
+                  <span class="statistic__details">Правильные ответы:</span><h5 class="statististic__details--align_left">${this.rightAnswers}</h5><br>
+                  <span class="statistic__details">Новые слова: </span><h5 class="statististic__details--align_left">${this.newWords}</h5><br>
+                  <span class="statistic__details">Самая длинная серия правильных ответов: </span><h5 class="statististic__details--align_left">${this.longSeriesCorrectAnswers}</h5>
+              </div>
+          </div>
+          <div class="graph" id="graph__id">123123</div>
+      </div> 
+    `;
+    }
+  }
+
+  async render() {
+    return this.view;
+  }
+
+  async afterRender() {
+    // apply data from API response
+    this.cardCompleted = '10';
+    this.rightAnswers = '10';
+    this.newWords = '10';
+    this.longSeriesCorrectAnswers = '30';
+    this.createStatistic();
+  }
 }
-}
-const s = new Statistics({
-  cardCompleted: '1',
-  rightAnswers: '1',
-  newWords: '2',
-  longSeriesCorrectAnswers: '3',
-});
-const content = document.querySelector('#page_container');
-content.append(s.createStatistic());
-export default new Statistics();
+
+export default new Statistics(
+  {
+    cardCompleted: '1',
+    rightAnswers: '1',
+    newWords: '2',
+    longSeriesCorrectAnswers: '3',
+  },
+);
